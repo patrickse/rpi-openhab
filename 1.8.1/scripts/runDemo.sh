e5ba629d19d78c4ba7dc0b738077484698e2c822
Patrick Sernetz <psernetz@cad-schroer.de>
2016-02-16 09:57:27 +0100

    Added setOption Function

--- 1.8.1/scripts/runDemo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cp -R /opt/openhab/demoData/* /opt/openhab/
-sh /opt/openhab/start.sh
--- 1.8.1/scripts/startOh.sh
+++ 1.8.1/scripts/startOh.sh
@@ -21,15 +21,23 @@ then
     cp /opt/openhab/configurations/openhab_default.cfg /opt/openhab/configurations/openhab.cfg >> /dev/null
 fi
 
-if [ -z "$OH_HOMEMATIC_HOST" ]; then
-sed "s/# homematic:host=/homematic:host=$OH_HOMEMATIC_HOST/" /opt/openhab/configurations/openhab.cfg > /opt/openhab/configurations/openhab.cfg
-fi
+setOption() {
+  if [ -n "$2" ]; then
+    sed -i -E "s/# $1=/$1=$2/" /opt/openhab/configurations/openhab.cfg
+    echo "\t$1=$2"
+  fi
+}
 
-#sed "s/# homematic:host=/homematic:host.timeout=$OH_HOMEMATIC_HOST_TIMEOUT/" /opt/openhab/configurations/openhab.cfg > /opt/openhab/configurations/openhab.cfg
-#sed "s/# homematic:host=/homematic:callback.host=$OH_HOMEMATIC_CALLBACK_HOST/" /opt/openhab/configurations/openhab.cfg > /opt/openhab/configurations/openhab.cfg
-#sed "s/# homematic:host=/homematic:callback.port=$OH_HOMEMATIC_CALLBACK_PORT/" /opt/openhab/configurations/openhab.cfg > /opt/openhab/configurations/openhab.cfg
-#sed "s/# homematic:host=/homematic:alive.interval=$OH_HOMEMATIC_ALIVE_INTERVAL/" /opt/openhab/configurations/openhab.cfg > /opt/openhab/configurations/openhab.cfg
-#sed "s/# homematic:host=/homematic:reconnect.interval=$OH_HOMEMATIC_RECONNECT_INTERVAL/" /opt/openhab/configurations/openhab.cfg > /opt/openhab/configurations/openhab.cfg
+echo "Update configuration"
+setOption homematic:host                $OH_HOMEMATIC_HOST
+setOption homematic:host.timeout        $OH_HOMEMATIC_HOST_TIMEOUT
+setOption homematic:callback.host       $OH_HOMEMATIC_CALLBACK_HOST
+setOption homematic:callback.port       $OH_HOMEMATIC_CALLBACK_PORT
+setOption homematic:alive.interval      $OH_HOMEMATIC_ALIVE_INTERVAL
+setOption homematic:reconnect.interval  $OH_HOMEMATIC_RECONNECT_INTERVAL
+
+echo "Disable File Logging"
+sed -i -E "s/<\appender\s.*FILE\s\///" /opt/openhab/configurations/logback.xml
 
 # Run openHAB
 sh /opt/openhab/start.sh
